fe77989359642b8f11b46ae1253dac57b720d7b2
nicex000 <martinlagas@hotmail.com>
2020-08-10 01:14:18 +0900

    Fix migration 😊

---
+++ Shimakaze-chan/Migrations/20200809161132_StreamingGuilds.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Shimakaze_chan.Migrations
+{
+    public partial class StreamingGuilds : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
