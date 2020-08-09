12929f7bc81a4e5691fe1256ad1c15b34becb6d3
Michał <30981101+michauorin@users.noreply.github.com>
2020-08-09 18:40:29 +0200

    reverse visual studio fucking with us

--- Shimakaze-chan/Migrations/20200809161132_StreamingGuilds.cs
+++ Shimakaze-chan/Migrations/20200809161132_StreamingGuilds.cs
@@ -6,12 +6,23 @@ namespace Shimakaze_chan.Migrations
     {
         protected override void Up(MigrationBuilder migrationBuilder)
         {
-
+            migrationBuilder.CreateTable(
+                name: "StreamingGuild",
+                columns: table => new
+                {
+                    GuildId = table.Column<decimal>(nullable: false),
+                    ChannelId = table.Column<decimal>(nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_StreamingGuild", x => x.GuildId);
+                });
         }
 
         protected override void Down(MigrationBuilder migrationBuilder)
         {
-
+            migrationBuilder.DropTable(
+                name: "StreamingGuild");
         }
     }
 }
